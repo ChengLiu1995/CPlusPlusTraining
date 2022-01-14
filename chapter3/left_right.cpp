e2d295dcaef96ac5af8f9a49f0a8bf173afecf50
liucheng28 <liucheng28@meituan.com>
2022-01-14 19:08:35 +0800

    left right plus

--- chapter3/left_right.cpp
+++ chapter3/left_right.cpp
@@ -9,15 +9,28 @@ using namespace std;
 // }
 // };
 
-void reference(std::string &str)
+// void reference(std::string &str)
+// {
+//     std::cout << " 左值" << std::endl;
+// }
+// void reference(std::string &&str)
+// {
+//     std::cout << " 右值" << std::endl;
+// }
+void reference(int &v)
 {
     std::cout << " 左值" << std::endl;
 }
-void reference(std::string &&str)
+void reference(int &&v)
 {
     std::cout << " 右值" << std::endl;
 }
-
+template <typename T>
+void pass(T &&v)
+{
+    std::cout << " 普通传参:";
+    reference(v); // 始终调用 reference(int&)
+}
 // void increase(int &v)
 // {
 //     v++;
@@ -80,4 +93,10 @@ int main()
     std::cout << "obj:" << std::endl;
     std::cout << obj.pointer << std::endl;
     std::cout << *obj.pointer << std::endl;
+
+    std::cout << " 传递右值:" << std::endl;
+    pass(1); // 1 是右值, 但输出是左值
+    std::cout << " 传递左值:" << std::endl;
+    int l = 1;
+    pass(l); // l 是左值, 输出左值
 }
